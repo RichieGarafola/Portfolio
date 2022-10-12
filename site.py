ccaa8254e1397d136d4c246539a208939920c05d
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2022-10-12 19:02:08 -0400

    Update site.py

--- site.py
+++ site.py
@@ -130,7 +130,7 @@ if selected == "Projects":
     with st.container():
         image_column, text_column = st.columns((1,2))
         with image_column:
-            st.image("./Images/ExcelSales.png")
+            st.image("./Images/ExcelSales.gif")
             
         with text_column:
             st.subheader("")
